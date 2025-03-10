fbba3c0a0880a3627ab5f4fc8bda1e7012c43092
alciz224 <alycisse473@gmail.com>
2025-03-10 08:52:53 +0100

    ccc

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
-import {routes} from './routes'
+
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import PrivateRoutes from "./pages/PrivateRoutes";
+import PrivatePage from "./pages/PrivatePage";
+
+const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <Login/>
+  },
+  {
+    path: "register",
+    element: <Register/>
+  },
+  {
+    path: "home",
+    index: true,
+    element: <Home/>
+  },
+  {
+    element: <PrivateRoutes/>,
+    children: [
+      {
+        path: "private",
+        element: <PrivatePage/>
+      }
+    ]
+  }
+]);
 
 const App = () => {
   return (
-    <RouterProvider router={routes}/>
+    <RouterProvider router={router}/>
   )
 }
 
